255b9de1d92894ab87e64bce6eda7ccc4d3129d0
Nathan Grotticelli <NathanGrotticelli@gmail.com>
2014-03-11 13:17:31 -0400

    seed4

--- seed.js
+++ seed.js
@@ -12,104 +12,110 @@ School.remove({}, function(err) {
 
 console.log("Inserting data");
 
+// var schoolUpload = function(){
+				var school = new School({
+				  schoolLongMax: '-75.4',
+				  schoolLongMin: '-76.1',
+				  schoolLatMax: '42.4',
+				  schoolLatMin: '41.7',
+				  schoolFriendMin: 60,
+				  schoolName: 'Binghamton',
+				  schoolTown: 'Binghamton',
+				  emailEnding: '@binghamton.edu',
+				  emailLength: 18 });
 
-var school = new School({
-  schoolLongMax: '-75.4',
-  schoolLongMin: '-76.1',
-  schoolLatMax: '42.4',
-  schoolLatMin: '41.7',
-  schoolFriendMin: 60,
-  schoolName: 'Binghamton',
-  schoolTown: 'Binghamton',
-  emailEnding: '@binghamton.edu',
-  emailLength: 18 });
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: SB');
+							  }
+							});
 
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: SB');
-			  }
-			});
+				var school = new School({
+				  schoolLongMax : "-76.8", schoolLongMin : "-77.3", schoolLatMax : "39.1", schoolLatMin : "38.4", schoolFriendMin : 60, schoolName : "George Washington University", schoolTown : "District of Columbia", emailEnding : "@gwu.edu", emailLength : 11 });
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: GW');
+							  }
+							});
 
-var school = new School({
-  schoolLongMax : "-76.8", schoolLongMin : "-77.3", schoolLatMax : "39.1", schoolLatMin : "38.4", schoolFriendMin : 60, schoolName : "George Washington University", schoolTown : "District of Columbia", emailEnding : "@gwu.edu", emailLength : 11 });
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: GW');
-			  }
-			});
+				var school = new School({schoolLongMax : "-74.8", schoolLongMin : "-75.4", schoolLatMax : "42.7", schoolLatMin : "42.1", schoolFriendMin : 60, schoolName : "Oneonta", schoolTown : "Oneonta", emailEnding : "@suny.oneonta.edu", emailLength : 20 });
 
-var school = new School({schoolLongMax : "-74.8", schoolLongMin : "-75.4", schoolLatMax : "42.7", schoolLatMin : "42.1", schoolFriendMin : 60, schoolName : "Oneonta", schoolTown : "Oneonta", emailEnding : "@suny.oneonta.edu", emailLength : 20 });
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: SO');
+							  }
+							});
 
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: SO');
-			  }
-			});
+				var school = new School({schoolLongMax : "-80.9", schoolLongMin : "-81.6", schoolLatMax : "28.9", schoolLatMin : "28.3", schoolFriendMin : 60, schoolName : "Central Florida", schoolTown : "Orlando", emailEnding : "@knights.ucf.edu", emailLength : 19});
 
-var school = new School({schoolLongMax : "-80.9", schoolLongMin : "-81.6", schoolLatMax : "28.9", schoolLatMin : "28.3", schoolFriendMin : 60, schoolName : "Central Florida", schoolTown : "Orlando", emailEnding : "@knights.ucf.edu", emailLength : 19});
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: CFU');
+							  }
+							});
 
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: CFU');
-			  }
-			});
+				var school = new School({schoolLongMax : "-84.22", schoolLongMin : "-84.64", schoolLatMax : "43", schoolLatMin : "42.55", schoolFriendMin : 60, schoolName : "Michigan State", schoolTown : "East Lansing", emailEnding : "@msu.edu", emailLength : 11 });
 
-var school = new School({schoolLongMax : "-84.22", schoolLongMin : "-84.64", schoolLatMax : "43", schoolLatMin : "42.55", schoolFriendMin : 60, schoolName : "Michigan State", schoolTown : "East Lansing", emailEnding : "@msu.edu", emailLength : 11 });
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: MS');
+							  }
+							});
 
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: MS');
-			  }
-			});
+				var school = new School({schoolLongMax : "-83.45", schoolLongMin : "-83.95", schoolLatMax : "42.5", schoolLatMin : "42", schoolFriendMin : 60, schoolName : "University of Michigan", schoolTown : "Ann Arbor", emailEnding : "@umich.edu", emailLength : 13});
 
-var school = new School({schoolLongMax : "-83.45", schoolLongMin : "-83.95", schoolLatMax : "42.5", schoolLatMin : "42", schoolFriendMin : 60, schoolName : "University of Michigan", schoolTown : "Ann Arbor", emailEnding : "@umich.edu", emailLength : 13});
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: UM');
+							  }
+							});
 
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: UM');
-			  }
-			});
+				var school = new School({schoolLongMax : "-84.65", schoolLongMin : "-84.99", schoolLatMax : "43.64", schoolLatMin : "43.35", schoolFriendMin : 60, schoolName : "Central Michigan", schoolTown : "Mt Pleasant", emailEnding : "@umich.edu", emailLength : 13});
 
-var school = new School({schoolLongMax : "-84.65", schoolLongMin : "-84.99", schoolLatMax : "43.64", schoolLatMin : "43.35", schoolFriendMin : 60, schoolName : "Central Michigan", schoolTown : "Mt Pleasant", emailEnding : "@umich.edu", emailLength : 13});
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: CMU');
+							  }
+							});
 
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: CMU');
-			  }
-			});
+				var school = new School({schoolLongMax : "-157.6", schoolLongMin : "-158", schoolLatMax : "21.49", schoolLatMin : "21.05", schoolFriendMin : 30, schoolName : "University of Hawaii", schoolTown : "Manoa", emailEnding : "@hawaii.edu", emailLength : 14 });
 
-var school = new School({schoolLongMax : "-157.6", schoolLongMin : "-158", schoolLatMax : "21.49", schoolLatMin : "21.05", schoolFriendMin : 30, schoolName : "University of Hawaii", schoolTown : "Manoa", emailEnding : "@hawaii.edu", emailLength : 14 });
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: UH');
+							  }
+							});
 
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: UH');
-			  }
-			});
+				var school = new School({ schoolLongMax : "-88", schoolLongMin : "-88.5", schoolLatMax : "40.5", schoolLatMin : "39.8", schoolFriendMin : 60, schoolName : "University of Illinois at Urbana-Champaign", schoolTown : "Champaign", emailEnding : "@illinois.edu", emailLength : 15 });
+				school.save(function (err, saved) {
+							  if (err){ return console.error(err);}
+							  else{
+							  	console.log('saved: UI');
+							  	process.exit();
+							  	// School.findOne({schoolName: 'University of Illinois at Urbana-Champaign'},function(req,res){
+										// 	console.log(res)
+										// });
+							  }
+							});
 
-var school = new School({ schoolLongMax : "-88", schoolLongMin : "-88.5", schoolLatMax : "40.5", schoolLatMin : "39.8", schoolFriendMin : 60, schoolName : "University of Illinois at Urbana-Champaign", schoolTown : "Champaign", emailEnding : "@illinois.edu", emailLength : 15 });
-school.save(function (err, saved) {
-			  if (err){ return console.error(err);}
-			  else{
-			  	console.log('saved: UI');
-			  	// School.findOne({schoolName: 'University of Illinois at Urbana-Champaign'},function(req,res){
-						// 	console.log(res)
-						// });
-			  }
-			});
+				// 	School.findOne({schoolName: 'Binghamton'},function(req,res){
+				// 	console.log(res)
+				// });
 
-// 	School.findOne({schoolName: 'Binghamton'},function(req,res){
-// 	console.log(res)
+// console.log("Control-C to quit");
+// };
+
+// schoolUpload(function(req,res){
+// 	console.log('done');
 // });
-
-console.log("Control-C to quit");
 
 });
 
