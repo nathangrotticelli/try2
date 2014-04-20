9bef731243357ce4d90340f469d817645b966e42
Nathan Grotticelli <NathanGrotticelli@gmail.com>
2014-04-20 00:33:35 -0400

    userPost3

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ var regExNums = /[0-9]/g;
 
 app.post('/userPost',function(req,res){
 			console.log('message good: '+req);
-			res.JSON({success:'Worked!'});
+			res.json({success:'Worked!'});
 		 	// User.findOneAndUpdate({userProfId: userProfId},
 		 	// 			{firstNameLetter: firstNameLetter,
 				// 	  schoolFriendCount: schoolFriendCount,
