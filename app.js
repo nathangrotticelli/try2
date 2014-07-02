56e16e9b15f264e9e679b8d2a11e8be92ae0efc4
nathangrotticelli <nathangrotticelli@gmail.com>
2014-07-02 00:31:37 -0400

    get user16

--- app.js
+++ app.js
@@ -325,7 +325,7 @@ User.findOne({ userEmail: userEmail}).exec(function (err, user) {
       // console.log(user);
        userItem = user;
        console.log('user is ',user.userName);
-       console.log('user is ',user.school);
+       console.log('user is ',user.userSchool);
        res.json({Item: userItem});
     }
     else{
