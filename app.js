459a5096cb1bfdef4e1c3d91c7ffba50208032bb
nathangrotticelli <nathangrotticelli@gmail.com>
2014-11-22 03:11:07 -0500

    priv event 21

--- app.js
+++ app.js
@@ -221,8 +221,9 @@ console.log('here22222232323232');
 // var a123 = "{
 //         "$oid": "54703c49e4b04de436f29363"
 //     }"
-// console.log(PrivateEvents.db.collections);
-console.log(PrivateEvents.db.collections);
+console.log(JSON.stringify(PrivateEvents));
+console.log('her999999999');
+console.log(PrivateEvents.db);
 // console.log(PrivateEvents.db.collections);
 
 // PrivateEvents.findOne({privateTag: "privateTag" }, function(err, pEvents){
