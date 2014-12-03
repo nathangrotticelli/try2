0820306db519c84f40fda92d763c3c8ed3d32b2c
nathangrotticelli <nathangrotticelli@gmail.com>
2014-12-03 08:55:52 -0500

    email fix3

--- app.js
+++ app.js
@@ -332,7 +332,7 @@ var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
         user: 'unrepteam@gmail.com',
-        pass: 'University'
+        pass: 'Lilbill666'
     }
 });
 
