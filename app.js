887bfa7324e69d867ac375852e68ed470f24d43b
nathangrotticelli <nathangrotticelli@gmail.com>
2014-12-03 07:40:39 -0500

    first login fina5

--- app.js
+++ app.js
@@ -369,7 +369,7 @@ userProfId = req.body.userProfId;
 for(z=0;z<fbFriends.length;z++){
   // console.log(fbFriends[z].userName);
   // otherUser.notifications.push({message:message,date:notDate})
-  message = "Your Facebook friend "+userName+" just joined U Nightlife. Tap this message to follow them!";
+  message = "Your Facebook friend "+userName+" just joined U Nightlife.";
   notDate = Date.now();
   tap = "follow";
   // fbFriends[z].notifications.push({message:message,date:notDate,tap:tap,followId:userProfId});
