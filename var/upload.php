7f2448fd051fbaa2c1ea4c438674302276439d25
nathangrotticelli <nathangrotticelli@gmail.com>
2015-01-22 10:05:45 -0500

    create watch user7

--- var/upload.php
+++ var/upload.php
@@ -1,5 +1,5 @@
 <?php
    print_r($_FILES);
    $new_image_name = "YEAH.jpg";
-   move_uploaded_file($_FILES["file"]["tmp_name"], "/var/TEST/".$new_image_name);
+   move_uploaded_file($_FILES["file"]["tmp_name"], "/var/www/TEST/".$new_image_name);
 ?>
