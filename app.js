09ed0b753297948dae9c95948065403d31fb04f1
nathangrotticelli <nathangrotticelli@gmail.com>
2015-01-24 07:58:12 -0500

    pic Upate8

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ app.post('/picGet', function(req,res){
 app.post('/picUpdate',function(req,res){
 console.log(req.body.username);
 
-WatchSchema.update({ listname: 'userList', username: req.body.username}, { 'user.userPic': req.body.userPic }, function(){
+WatchSchema.update({ listname: 'userList'}, { 'user.userPic': req.body.userPic }, function(){
   console.log('worked like a charm');
   res.json({success:'Worked!'});
 })
@@ -291,10 +291,19 @@ app.post('/createUser',function(req,res){
      var user = req.body;
      user.likes = [];
      user.collections = [];
+     // var myObj = ...;
+WatchSchema.findOneAndUpdate(
+    {username: req.body.username},
+    req.body,
+    {upsert: true},
+    function() {
+      console.log('worked');
+    }
+);
      //   console.log(req.body.userPic);
 
             WatchSchema.update({listName: "userList"},
-              {$push: {users:user}},
+              {$push: {users.user:user}},
             function(err){
               if(err){console.log(err.message)
                   res.json({failed:'failed'});
