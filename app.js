efffd9bc006eb3aa4fbe8259b4a12d999d69bdc0
nathangrotticelli <nathangrotticelli@gmail.com>
2015-01-26 05:40:19 -0500

    likked21

--- app.js
+++ app.js
@@ -284,25 +284,21 @@ app.post('/liked', function(req,res){
     //     }
     //         });
 
-  WatchSchema.update({'watchesIndex.watchName': watch.watchName},
-            {'$push': {'watchesIndex.$.watchLikes': user}},function(err2,worked2) {
+  WatchSchema.update({'watchesIndex.watchName': watch.watchName},{'$push': {'watchesIndex.$.watchLikes': user}},function(err2,worked2) {
               if(err2){
                     console.log(err2);
               }else{
                 console.log('watch like updated.');
-                 WatchSchema.update({'users.username': user.username},{'$push': {'users.$.likes': watch}},function(err,worked){
-                      if(err){
-                            console.log(err);
-                      }else{
-                          console.log('user like updated.');
-
-                      res.json(200);
-                      }
-                  });
+                 // WatchSchema.update({'users.username': user.username},{'$push': {'users.$.likes': watch}},function(err,worked){
+                 //      if(err){
+                 //            console.log(err);
+                 //      }else{
+                 //          console.log('user like updated.');
+                 //          res.json(200);
+                 //      }
+                 //  });
               }
           });
-
-
 });
 
 app.post('/picUpdate',function(req,res){
