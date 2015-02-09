12601f5623ee574e2713b01e9ecf0522989509d1
nathangrotticelli <nathangrotticelli@gmail.com>
2015-02-09 18:04:51 -0500

    user log in3

--- app.js
+++ app.js
@@ -273,6 +273,7 @@ app.post('/getUser22', function(req,res){
               }
             // }
         }
+         res.json({user:'false'});
             // console.log('Got Watches!');
             // var watchIndex = watchList.watchIndex;
             // res.json({user: userList.users[userList.users.indexOf(user)]});
