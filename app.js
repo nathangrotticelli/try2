bf283d23600ec3195f6e209c48fb06348630bb96
nathangrotticelli <nathangrotticelli@gmail.com>
2015-02-25 04:33:41 -0500

    watch collection 3

--- app.js
+++ app.js
@@ -406,7 +406,7 @@ app.post('/addCollection', function(req,res){
  var user = req.body.user;
  var collectionName = req.body.collectionName;
 
-  WatchSchema.update({'users.collections.collectionName': collectionName},{'$push': {'users.$.collections.watches': watch.watchName } },function(err1){
+  WatchSchema.update({'collections.collectionName': collectionName},{'$push': {'users.$.collections.watches': watch.watchName } },function(err1){
               if(err1){
                     console.log(err1);
               }else{
